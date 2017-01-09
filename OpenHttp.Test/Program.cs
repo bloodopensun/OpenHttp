616b2e2f7dc806adad26c54fe1ca7e3d4427770b
血开阳 <bloodopensun@gmail.com>
2017-01-09 20:19:49 +0800

    1.0

---
+++ OpenHttp.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenHttp.Test
+{
+    class Program
+    {
+        private static HttpClient _httpClient = new HttpClient();
+        static void Main(string[] args)
+        {
+            var head = HttpHead.Builder
+                .Url("http://www.baidu.com")
+                .MethodGet()
+                    .AddData("wd", "GooGle")
+                .End();
+            var str = _httpClient.Load(ref head).ToString(head.Encod);
+        }
+    }
+}
